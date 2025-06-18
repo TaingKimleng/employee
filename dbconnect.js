fb4933760c837fcda584d1c5324fb28ac7128303
TaingKimleng <156338322+TaingKimleng@users.noreply.github.com>
2025-06-18 21:58:30 +0700

    Update dbconnect.js

--- dbconnect.js
+++ dbconnect.js
@@ -1,22 +1,28 @@
 // dbconnect.js
 const mongoose = require('mongoose');
-const uri = process.env.MONGO_URI || "mongodb+srv://kimleng:<db_password>@...";
+
+const uri = process.env.MONGO_URI || "mongodb+srv://kimleng:<db_password>@midterm.hniszn4.mongodb.net/?retryWrites=true&w=majority&appName=midterm";
 
 let isConnected = false;
 
-async function connectDB() {
-  if (isConnected) return;
+const connectDB = async () => {
+  if (isConnected) {
+    console.log('🔁 Already connected to MongoDB');
+    return;
+  }
+
   try {
     await mongoose.connect(uri, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
+
     isConnected = true;
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection error:', err.message);
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
